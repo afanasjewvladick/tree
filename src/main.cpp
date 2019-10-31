68fc8765e28f9d24c0bff00c4e5ef3fc25b11893
Vladick Afanasjew <afanasjew.vladick@yandex.ru>
2019-10-31 21:42:51 +0300

    Получение текущего коталога

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,26 @@
 
 using namespace std;
 
-int main() {
-  cout << "Hello world" << endl;
-};
+#include <stdio.h>  /* defines FILENAME_MAX */
+// #define WINDOWS  /* uncomment this line to use it for windows.*/ 
+#ifdef WINDOWS
+#include <direct.h>
+#define GetCurrentDir _getcwd
+#else
+#include <unistd.h>
+#define GetCurrentDir getcwd
+#endif
+ 
+string GetCurrentWorkingDir(void) {
+  char buff[FILENAME_MAX];
+  GetCurrentDir( buff, FILENAME_MAX );
+  string current_working_dir(buff);
+  return current_working_dir;
+}
+ 
+int main(){
+  cout << GetCurrentWorkingDir() << endl;
+  return 1;
+}
 
 
