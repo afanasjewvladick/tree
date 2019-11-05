beffd9aa5cba753108fa012f899b51347f531a67
Vladick Afanasjew <afanasjew.vladick@yandex.ru>
2019-11-05 23:16:59 +0300

    Отображение содержимого каталога

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
-
-using namespace std;
-
-#include <stdio.h>  /* defines FILENAME_MAX */
-// #define WINDOWS  /* uncomment this line to use it for windows.*/ 
+#include <sys/types.h>
+#include <dirent.h>
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h>
 #ifdef WINDOWS
 #include <direct.h>
 #define GetCurrentDir _getcwd
 #else
-#include <unistd.h>
 #define GetCurrentDir getcwd
 #endif
+
+using namespace std;
  
 string GetCurrentWorkingDir(void) {
   char buff[FILENAME_MAX];
@@ -20,8 +21,22 @@ string GetCurrentWorkingDir(void) {
 }
  
 int main(){
+  // выводить текущую директорию
   cout << GetCurrentWorkingDir() << endl;
+  // Открывает коталог
+  DIR *dir = opendir(".");
+    if(dir)
+    {
+        struct dirent *ent;
+        while((ent = readdir(dir)) != NULL)
+        {
+          printf( "%s %s\n", "├── " ,ent->d_name );
+        }
+    }
+    else
+    {
+        fprintf(stderr, "Error opening directory\n");
+    }
   return 1;
 }
-
 
